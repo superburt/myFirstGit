ae11cfa39439624ecbbb3db9347d630ee66c7623
superburt <53563328+superburt@users.noreply.github.com>
2020-07-25 14:10:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log("this is a merge conflict I created");
 //this is a comment so I can git push origin master :) 
+
+a=2
+b=3
+
+function sum(a, b) {
+    return a + b;
+  }
+
+
+console.log(sum(5, 10)); 
